d960b4f928a3ae1e3aeb33352f814313ab987ea2
Khodani Tshisimba <khodanitshisimba@gmail.com>
2026-04-30 01:51:12 +0200

    Fix indentation bug

--- backend/hangry_api/tests/test_DeliveryCost.py
+++ backend/hangry_api/tests/test_DeliveryCost.py
@@ -27,11 +27,11 @@ def test_MiddleOfTheRoadItems():
 
 def test_LittleItems():
   #Arrange
-order = MockSet()
-order.add(MockModel(quantity=3))
-order.add(MockModel(quantity=1))
-del_dist = 2
-#Act
-cost = Delivery.calculate(order, del_dist)
-#Assert
-assert cost == 2.50
+  order = MockSet()
+  order.add(MockModel(quantity=3))
+  order.add(MockModel(quantity=1))
+  del_dist = 2
+  #Act
+  cost = Delivery.calculate(order, del_dist)
+  #Assert
+  assert cost == 2.50
